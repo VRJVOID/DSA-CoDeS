c6ab1555fb238ccaba65735ce712609892ca32dc
V0id <86529756+VRJVOID@users.noreply.github.com>
2023-11-06 00:03:28 +0530

    Add files via upload

---
+++ rotateByD.java
@@ -0,0 +1,12 @@
+public class rotate{
+    public static void main(String[] args) {
+        int temp;
+        int arr[] = {1,2,3,4,5};
+        int k=2;
+        for(int i=0;i<k;i++)
+        {
+            temp[i] = arr[i];
+        }
+        System.out.println(temp[i]);
+    }
+}
